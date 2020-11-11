294965e51fb15c9c5158fcec491b947dc13738d8
Christian Valdez <christianvaldez36@hotmail.com>
2020-11-10 18:15:54 -0800

    fixed casing of docker username

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t unChartedZone/multi-client:latest -t unChartedZone/multi-client:$SHA -f ./client/Dockerfile ./client
-docker built -t unChartedZone/multi-server:latest -t unChartedZone/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t unChartedZone/multi-worker:latest -t unChartedZone/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t unchartedzone/multi-client:latest -t unchartedzone/multi-client:$SHA -f ./client/Dockerfile ./client
+docker built -t unchartedzone/multi-server:latest -t unchartedzone/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t unchartedzone/multi-worker:latest -t unchartedzone/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push unChartedZone/multi-client:latest
-docker push unChartedZone/multi-server:latest
-docker push unChartedZone/multi-worker:latest
+docker push unchartedzone/multi-client:latest
+docker push unchartedzone/multi-server:latest
+docker push unchartedzone/multi-worker:latest
 
-docker push unChartedZone/multi-client:$SHA
-docker push unChartedZone/multi-server:$SHA
-docker push unChartedZone/multi-worker:$SHA
+docker push unchartedzone/multi-client:$SHA
+docker push unchartedzone/multi-server:$SHA
+docker push unchartedzone/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=unChartedZone/multi-server:$SHA
-kubectl set image deployments/client-deployment client=unChartedZone/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=unChartedZone/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=unchartedzone/multi-server:$SHA
+kubectl set image deployments/client-deployment client=unchartedzone/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=unchartedzone/multi-worker:$SHA
