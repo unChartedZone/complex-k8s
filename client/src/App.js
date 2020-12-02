e7e5ecabfbac97fb2533ba2806c7d178fbafbd10
Christian Valdez <christianvaldez36@hotmail.com>
2020-12-01 16:10:52 -0800

    fixed routing in client
    - downgraded react router version

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BroswerRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
 import OtherPage from "./OtherPage";
 import Fib from "./Fib";
+// import Home from "./Home";
+
+const LinkStyles = {
+  color: "lightblue",
+};
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+    <div className="App">
+      <Router>
         <div>
-          <Route exact path="/" component={Fib} />
-          <Route exact path="/otherpage" component={OtherPage} />
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            <Link style={LinkStyles} to="/otherpage">
+              Other Page
+            </Link>
+          </header>
+          <Switch>
+            <Route exact path="/" component={Fib} />
+            <Route exact path="/otherpage" component={OtherPage} />
+          </Switch>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
