0a8bb5e5a92cba0d7001ddf537a156a4224fa361
Christian Valdez <christianvaldez36@hotmail.com>
2020-12-01 16:13:59 -0800

    tweaked some styles of client pages

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default () => {
+const OtherPageStyles = {
+  margin: "10rem 0",
+};
+
+const OtherPage = () => {
   return (
-    <div>
+    <div style={OtherPageStyles}>
       Im some other page!
       <Link to="/">Go back home</Link>
     </div>
   );
 };
+
+export default OtherPage;
